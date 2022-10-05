d51a2a022afeffddf588383ada0858a3fb80e0d7
solbae <sol4854@naver.com>
2022-10-05 10:19:15 +0900

    계산기 클래스 작성

---
+++ Week3/src/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+    public void plus(int first, int second){
+        System.out.println(first + second);
+    }
+
+    public void minus(int first, int second){
+        System.out.println(first - second);
+    }
+
+    public void mod(int first, int second){
+        System.out.println(first / second);
+    }
+
+    public void div(int first, int second){
+        System.out.println(first % second);
+    }
+
+}
---
+++ Week3/src/CalculatorMain.java
@@ -0,0 +1,9 @@
+public class CalculatorMain {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.plus(10, 20);
+        calculator.minus(10, 20);
+        calculator.mod(10, 20);
+        calculator.div(10, 20);
+    }
+}
