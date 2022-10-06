962a0e0a7ce3695748e79196c5d9c214ecaf544f
solbae <sol4854@naver.com>
2022-10-06 14:42:33 +0900

    File을 n글자 읽는 메소드 작성

--- Week3/src/Thu221006/File/ReadFile.java
+++ Week3/src/Thu221006/File/ReadFile.java
@@ -2,13 +2,21 @@ package Thu221006.File;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.util.Scanner;
 
 public class ReadFile {
     public static void main(String[] args) throws IOException {
-
+        Scanner sc = new Scanner(System.in);
 
         FileReader reader = new FileReader("./a_file.txt");
-        char c = (char) reader.read();
-        System.out.println(c);
+
+        //char c = (char) reader.read();
+
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            System.out.println((char)reader.read());
+        }
+
     }
 }
