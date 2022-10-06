396b223bfd05a63512faeb53ba3f0eff2287e845
solbae <sol4854@naver.com>
2022-10-06 15:31:32 +0900

    현재 디렉토리의 파일 목록 읽기 메소드, 파일 내용을 한글자씩 받는 메소드를 main 밖에서 구현

--- Week3/src/Thu221006/File/FIleExercise.java
+++ Week3/src/Thu221006/File/FIleExercise.java
@@ -3,27 +3,31 @@ package Thu221006.File;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class FIleExercise {
-    public static void main(String[] args) throws FileNotFoundException {
-        //FileReader fileReader = new FileReader("./aa_file.txt");
 
-        File dir = new File("./");
+    //파일 목록을 확인하는 메소드
+    public  void printFiles(){
+        File dir = new File("./"); // pathname위치의 파일들을 전부 불러온다.
         File files[] = dir.listFiles();
         //System.out.println(files[0]);
-        for (File file : files){
+        for (File file : files) {
             System.out.println(file);
         }
     }
-    /*
-    .\.git
-    .\.idea
-    .\aa_file.txt
-    .\a_file.txt
-    .\InStudyingCodeup
-    .\Like_lion.iml
-    .\out
-    .\README.md
-    .\Week3
-     */
+
+    public char readAChar(String filename) throws IOException {
+        FileReader fileReader = new FileReader(filename);
+
+      //  System.out.println((char) fileReader.read());
+        return (char) fileReader.read();
+    }
+
+    public static void main(String[] args) throws IOException {
+        FIleExercise fIleExercise = new FIleExercise();
+        char c = fIleExercise.readAChar("aa_file.txt");
+        System.out.println(c);
+    }
 }
+
