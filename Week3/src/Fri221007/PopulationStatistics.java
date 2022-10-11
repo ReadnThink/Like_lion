98a6cbab04d10b76f4628151404978271eca8e11
solbae <sol4854@naver.com>
2022-10-11 16:45:02 +0900

    인구조사 프로젝트 주석 변경

--- Week3/src/Fri221007/PopulationStatistics.java
+++ Week3/src/Fri221007/PopulationStatistics.java
@@ -58,24 +58,28 @@ public class PopulationStatistics {
 
 
     public static void main(String[] args) throws IOException {
+        //인구이동을 확인하려면 전출, 전입 정보가 필요하여 데이터 2개필요. 구조체를 ArrayList와 같이 활용하였다.
         PopulationStatistics ps = new PopulationStatistics();
         List<PopulationMove> pm = new ArrayList<>();
+
+        // 파일위치를 저장하는 변수
         String filename = "C:\\Users\\iser\\Desktop\\멋사 교육자료\\자료\\2021_인구관련연간자료_20220927_66125.csv";
         String newfile = "C:\\Users\\iser\\Desktop\\멋사 교육자료\\자료\\From_to.txt";
 
         //구조체 배열(pm) 에 split된 전출, 전입 정보 담기
-        pm = ps.readLine(filename);
+        pm = ps.readLine(filename); //readLine 메소드에 parse(전입, 전출 정보만 저장하는 역할) 메소드 연결
 
         //strs에 (String배열) pm정보 옮기기
-        // (writer를 통해 새 파일에 전출, 전입 정보 옮기기 위해)
+        // 왜 구조체배열 pm에 있는 정보를 새파일에 옮기지 않는가? = 어렵다... /방법이 있는지 계속 공부해야함.
+        //writer를 통해 새 파일에 전출, 전입 정보 옮기기 위해
         List<String> strs = new ArrayList<>();
         for (PopulationMove populationMove : pm) {
-            strs.add(ps.fromToString(populationMove));
+            strs.add(ps.fromToString(populationMove)); //fromToString = pm에 있는 정보를 strs에 저장
         }
 
         //split된  str배열을 통해 전입 전출 정보가 있는 새 파일 생성
-        ps.FileCreate(newfile);
-        ps.WriteFile(strs,newfile);
+        ps.FileCreate(newfile); //파일생성
+        ps.WriteFile(strs,newfile); //생성파일에 Strs(전출, 전입) 저장.
 
         //map을 통해 중복된 전입, 전출정보를 카운트.
         MoveCount moveCount = new MoveCount();
@@ -90,17 +94,19 @@ public class PopulationStatistics {
 
 //        System.out.println(heatMapIdx);
 
-        //heatmap 파일 만들기
+        //최종 heatmap 파일 만들기 위해 구조체에 있는것을 String으로 전달.
         List<String> cntResult = new ArrayList<>();
-        for (String key : moveCntMap.keySet()) {
+        for (String key : moveCntMap.keySet()) { // moveCnt의 한줄씩 뽑아낸다.
             String[] fromto = key.split(",");
             String s = String.format("[%s, %s, %d],\n", heatMapIdx.get(fromto[0]),heatMapIdx.get(fromto[1]), moveCntMap.get(key));
-            cntResult.add(s); //hashmap으로 카운트 한 것을 반영
+            cntResult.add(s); //hashmap을 사용하기위해 중복 카운트 한 것을 반영
         }
-        Collections.sort(cntResult);
 
-        System.out.println(cntResult);
+        Collections.sort(cntResult); // 보기좋게 정렬
 
+        System.out.println(cntResult); // 잘되었는지 확인
+
+        // 새로운 파일 생성
         String FinalHeatMap = "C:\\Users\\iser\\Desktop\\멋사 교육자료\\자료\\for_heatmap.txt";
         ps.WriteFile(cntResult,FinalHeatMap);
 //        CreateFile createFile = new CreateFile();
