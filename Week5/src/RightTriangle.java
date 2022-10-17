3a34e644fa10d83a2842ed941204d26f64f57f85
solbae <sol4854@naver.com>
2022-10-17 09:36:14 +0900

    별찍기 constructor 오버로딩하여 입력한 문자 출력하기

--- Week5/src/RightTriangle.java
+++ Week5/src/RightTriangle.java
@@ -1,16 +1,28 @@
 import java.util.Scanner;
 
 public class RightTriangle {
-    public static void main(String[] args) {
+    private String letter = "*";
+
+    public RightTriangle(){}
+
+    public RightTriangle(String letter) {
+        this.letter = letter;
+    }
+
+    public void printStar(){
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
 
         for(int i = 0; i < n; i++){
             for (int j = 0; j <= i; j++){
-                System.out.print("*");
+                System.out.print(this.letter);
             }
             System.out.println();
         }
+    }
+    public static void main(String[] args) {
+        RightTriangle rt = new RightTriangle("@");
+        rt.printStar();
     }
 }
