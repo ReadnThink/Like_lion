4e1d2a046929f85c98a6cd0f344505b5fc1a3bc7
solbae <sol4854@naver.com>
2022-10-17 10:28:09 +0900

    n탑 피라미드 출력(홀수개만 출력 강사님 코드)

---
+++ Week5/src/PyramidTeachers.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class PyramidTeachers {
+    private String letter = "*";
+
+    public PyramidTeachers(){}
+
+    public PyramidTeachers(String letter) {
+        this.letter = letter;
+    }
+
+    public void printStar(){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+
+            for(int j = i; j < n - 1; j++){
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k < 2 * i + 1; k++) { //홀수개 출력
+                System.out.print(this.letter);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        PyramidTeachers pyramid = new PyramidTeachers();
+        pyramid.printStar();
+    }
+}
