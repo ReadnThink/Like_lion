1119cb86abdb6abcf2ce3c84b5fa03b5c2bc6696
solbae <sol4854@naver.com>
2022-10-18 09:18:12 +0900

    n 정사각형 출력

---
+++ Week5/src/RectangleStar.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class RectangleStar {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt() ;
+
+        makeRectangle(x, y);
+    }
+
+    private static void makeRectangle(int x,int y) {
+
+        for(int i = 0; i < x; i++){
+            for(int j = 0; j < y; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
