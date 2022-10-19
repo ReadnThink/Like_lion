3275004747e21f644b3003b3b432d5cd643f8ba6
solbae <sol4854@naver.com>
2022-10-19 14:19:51 +0900

    Spring 적용

--- UserDao/src/main/java/com/dao/DaoFactory.java
+++ UserDao/src/main/java/com/dao/DaoFactory.java
@@ -1,13 +1,19 @@
 package com.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+    @Bean
     private UserDao connectionMaker(){
         LocalConnectionMaker localConnectionMaker = new LocalConnectionMaker();
+        // context : 재사용 하는 부분이 많은 코드
         UserDao userDao = new UserDao(localConnectionMaker);
         return userDao;
 
     }
-
+    @Bean
     public UserDao userDao(){
         AWSConnectionMaker awsConnectionMaker = new AWSConnectionMaker();
         UserDao userDao = new UserDao(awsConnectionMaker);
--- UserDao/src/test/java/com/dao/UserdaoTest.java
+++ UserDao/src/test/java/com/dao/UserdaoTest.java
@@ -2,30 +2,44 @@ package com.dao;
 
 import com.domain.User;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = DaoFactory.class) //해당 클래스만 특정해서 불러온다.
 class UserdaoTest {
+    @Autowired
+    ApplicationContext context;
+
     @Test
     void addAndGet() throws SQLException, ClassNotFoundException {
-        DaoFactory daoFactory = new DaoFactory();
-        UserDao userdao = new DaoFactory().userDao();
+        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
+        UserDao userDao2 = context.getBean("awsUserDao", UserDao.class);
+
+        System.out.println(userDao);
+        System.out.println(userDao2);
+
         User user = new User();
 
         String id = "4";
 
-        user.setId(id);
-        user.setName("sols");
-        user.setPassword("1123");
-
-        userdao.add(user);
-
-        User result = userdao.select(id);
-        System.out.println(result.getId());
-        System.out.println(result.getName());
-        assertEquals(id, result.getId());
+//        user.setId(id);
+//        user.setName("sols");
+//        user.setPassword("1123");
+//
+//        userDao.add(user);
+//
+//        User result = userDao.select(id);
+//        System.out.println(result.getId());
+//        System.out.println(result.getName());
+//        assertEquals(id, result.getId());
 
 
     }
