25b86ff107195be16284707d11b95ff024a97d57
solbae <sol4854@naver.com>
2022-10-20 10:28:05 +0900

    Stack isEmpty, isFull 구현 Test케이스까지 완료

--- DataStructure-Java/src/main/java/Stack/Stack02.java
+++ DataStructure-Java/src/main/java/Stack/Stack02.java
@@ -1,9 +1,12 @@
 package Stack;
 
+import java.util.EmptyStackException;
+
 public class Stack02 {
 
     private Integer[] arr;
     private int top = -1;
+
 
 
     public Stack02() {
@@ -19,13 +22,26 @@ public class Stack02 {
     }
 
     public void push(int value){
+        if(isFull()){
+            throw new RuntimeException("스택이 꽉차있습니다");
+        }
         this.arr[++top] = value;
     }
 
 
     public int pop() {
-        int value = this.arr[top--];
+        if(isEmpty()){
+            throw new RuntimeException("스택이 비어있습니다.");
+        }
+        return this.arr[top--];
+    }
 
-        return value;
+    public boolean isEmpty() {
+        return this.top == -1;
+    }
+
+
+    public boolean isFull() {
+        return this.top == this.arr.length -1;
     }
 }
--- DataStructure-Java/src/test/java/Stack/Stack02Test.java
+++ DataStructure-Java/src/test/java/Stack/Stack02Test.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Stack;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class Stack02Test {
-
+    Stack02 stack02 = new Stack02();
     @BeforeEach
     void setUp(){
         // ex) db에서 데이터 지우는 코드
@@ -18,7 +20,7 @@ class Stack02Test {
 
     @Test
     void pushTest() {
-        Stack02 stack02 = new Stack02();
+
         stack02.push(10);
         stack02.push(20);
 
@@ -30,13 +32,38 @@ class Stack02Test {
     @Test
     @DisplayName("push, pop이 잘 되었는지 확인")
     void pushAndPop(){
-        Stack02 stack02 = new Stack02();
         stack02.push(10);
         stack02.push(20);
 
         assertEquals(20,stack02.pop());
         assertEquals(10,stack02.pop());
-
+        assertThrows(RuntimeException.class, () ->{
+            stack02.pop();
+        });
 
     }
+
+    @Test
+    @DisplayName("isEmpty")
+    void isEmpty(){
+        Stack02 stack02 = new Stack02();
+        assertTrue(stack02.isEmpty());
+        stack02.push(10);
+        assertFalse(stack02.isEmpty());
+        stack02.pop();
+        assertTrue(stack02.isEmpty());
+    }
+
+    @Test
+    void isFull(){
+        Stack02 stack02 = new Stack02(2);
+        stack02.push(10);
+        assertFalse(stack02.isFull());
+        stack02.push(10);
+        assertTrue(stack02.isFull());
+        assertThrows(RuntimeException.class, () ->{
+            stack02.push(20);
+        });
+    }
+
 }
